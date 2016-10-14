6ab0ce9827470b939b93c54448bc99ebfc035a46
auminus <jonnystarr@gmail.com>
2016-10-14 02:52:41 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,43 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var roleSlave = require('role.slave');
 
 module.exports.loop = function () {
     
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
     
-    if(harvesters.length < 2) {
+    if(harvesters.length < 1) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
+    
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     console.log('Upgraders: ' + upgraders.length);
     
-    if(upgraders.length < 3) {
+    if(upgraders.length < 2) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new upgrader: ' + newName);
     }
-var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+    
+    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     console.log('Builders: ' + builders.length);
     
-    if(builders.length < 2) {
+    if(builders.length < 0) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
         console.log('Spawning new builder: ' + newName);
     }
+    
+     var slaves = _.filter(Game.creeps, (creep) => creep.memory.role == 'slave');
+    console.log('Slaves: ' + slaves.length);
+    
+    if(slaves.length < 1) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'slave'});
+        console.log('Spawning new slave: ' + newName);
+    }
 
-    var tower = Game.getObjectById('57fddedc4172f5894f432307');
+    var tower = Game.getObjectById('TOWER_ID');
     if(tower) {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => structure.hits < structure.hitsMax
@@ -51,6 +62,9 @@ var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
         }
         if(creep.memory.role == 'builder') {
             roleBuilder.run(creep);
+        }
+        if(creep.memory.role == 'slave') {
+            roleSlave.run(creep);
         }
     }
 }
