0110e45dab568b29ea2b3cf67271f5f2ba81aa93
auminus <jonnystarr@gmail.com>
2016-10-14 02:57:19 -0700

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -9,21 +9,13 @@ var roleHarvester = {
             }
         }
         else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                    }
-            });
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENTION) && structure.energy < structure.energyCapacity;
-                    }
-            });
+
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
                     }
             });
+            
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
